4d4b35925edc35d6662647f32967a4592dd957c6
Enes Malik Terzi <enes.terzi@snapmuse.com>
2025-07-12 23:50:57 +0300

    git changes

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,4 +1,8 @@
 const {ethers} = require("hardhat");
 module.exports = [
-   "0xcF632eAb35B4885C0f87D14cce44F574eACABFFb"
+    "0x64f9dB0942949d675Ffd694b840F509457Dc7bE2",
+    5,
+    ethers.parseEther("1"),
+    "0xcF632eAb35B4885C0f87D14cce44F574eACABFFb",
+    "0xcF632eAb35B4885C0f87D14cce44F574eACABFFb"
 ];
--- scripts/deploy-custom.ts
+++ scripts/deploy-custom.ts
@@ -4,11 +4,10 @@ async function main() {
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying Bridge...");
-    const tokenManagerAddress = "0x64f9dB0942949d675Ffd694b840F509457Dc7bE2"
-    const oracleAddress = "0xcF632eAb35B4885C0f87D14cce44F574eACABFFb";
+    const tokenManagerAddress = "0x0B3547CD0E14e7D42f8921b0c370FdFD708bff6C"
+    const oracleAddress = "0x27C0Eb7Ba9c334d0740Ff529589bd89007398883";
 
     const BridgeFactory = await ethers.getContractFactory("Bridge");
-
     const transferFee = 5; // 1% (100 basis points)
     const operationFee = ethers.parseEther("1"); // 1 MRLN
 
